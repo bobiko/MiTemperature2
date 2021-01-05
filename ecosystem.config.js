1cabb3ef093f84d3f3f47b4dcec0e84634684aa3
bobiko <me@bobiko.pl>
2021-01-05 11:36:45 +0100

    CHANGED

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,25 +1,25 @@
 module.exports = {
-  apps : [
-{
-        "name": "MiTemperature2Office",
-        "script": "/home/pi/projekty/MiTemperature2/LYWSD03MMC.py",
-"args": ["-d A4:C1:38:1A:59:62 -2p","-p2 75","-o2","-4","-p1 33","-o1","-6","-r","--debounce","--skipidentical 50","--name MiOffice","--callback sendToInflux2.sh"],
-        "wait_ready": true,
-        "autorestart": true,
-        "max_restarts": 5,
-        "interpreter" : "/usr/bin/python3",
+  apps: [
+    {
+      "name": "MiTemperature2Office",
+      "script": "/home/pi/projekty/MiTemperature2/LYWSD03MMC.py",
+      "args": ["-- -d A4:C1:38:1A:59:62 -2p", "-p2 75", "-o2", "-4", "-p1 33", "-o1", "-6", "-r", "--debounce", "--skipidentical 50", "--name MiOffice", "--callback sendToInflux2.sh"],
+      "wait_ready": true,
+      "autorestart": true,
+      "max_restarts": 5,
+      "interpreter": "/usr/bin/python3",
     }
-],
+  ],
 
-  deploy : {
-    production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
-      ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
+  deploy: {
+    production: {
+      user: 'SSH_USERNAME',
+      host: 'SSH_HOSTMACHINE',
+      ref: 'origin/master',
+      repo: 'GIT_REPOSITORY',
+      path: 'DESTINATION_PATH',
       'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
       'pre-setup': ''
     }
   }
