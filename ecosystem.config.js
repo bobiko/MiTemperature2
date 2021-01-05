1037aac55387fd241031f5de44d81fc81ef03420
bobiko <me@bobiko.pl>
2021-01-05 11:56:47 +0100

    CHANGED

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,8 @@ module.exports = {
   apps: [
     {
       "name": "MiTemperature2Office",
-      "script": "/home/pi/projekty/MiTemperature2/LYWSD03MMC.py",
-      "args": ["-d='A4:C1:38:1A:59:62' -2p -p2='75' -o2 -4 -p1='33' -o1 -6 -r --debounce --skipidentical='50' --name='Office' --callback='sendToInflux2.sh'"],
+      "script": "/home/pi/projekty/MiTemperature2/LYWSD03MMC.py -d='A4:C1:38:1A:59:62' -2p - p2='75' - o2 - 4 - p1='33' - o1 - 6 - r--debounce--skipidentical = '50' --name = 'Office' --callback = 'sendToInflux2.sh'",
+      "args": [""],
       "wait_ready": true,
       "autorestart": true,
       "max_restarts": 5,
