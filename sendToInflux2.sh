f6f7c8e176e5d6b40ac68855fcce3e7050192418
bobiko <me@bobiko.pl>
2023-01-16 09:38:00 +0000

    backup

--- sendToInflux2.sh
+++ sendToInflux2.sh
@@ -5,6 +5,6 @@
 #DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 #$DIR/LYWSD03MMC.py -d <device> -2p -p2 75 -o2 -4 -p1 33 -o1 -6 -r --debounce --skipidentical 50 --name MySensor --callback sendToInflux.sh
 
-curl --request POST "http://192.168.8.2:8086/api/v2/write?org=bobiko&bucket=sensors&precision=s" \
---header "Authorization: Token bbSji2EmsBdHkW4F9Sncu8TqNR3v1NxZZPvf2-DdmttzTxTq8KRnXXV67SkF1PWEuTb_VMyVgV9bUNdGJzXP0w==" \
+curl --request POST "http://192.168.8.132:8086/api/v2/write?org=homelab-org&bucket=sensors&precision=s" \
+--header "Authorization: Token 5up3r-S3cr3t-auth-t0k3n" \
 --data-binary "AquaraBluetoothSensors,sensorname=$2 temperature=$3,calibratedHumidity=$6,voltage=$5,humidity=$4,batteryLevel=$7 $8"
